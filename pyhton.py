02c3f6ea19c33bc304e669a3e19a719e9edc60ac
leo torres <leo.torres@utec.edu.pe>
2025-05-03 21:21:44 -0500

    Modificacion de codigo en python

--- pyhton.py
+++ pyhton.py
@@ -1,7 +1,11 @@
 # Este es el primer archivo en python
-a=3
-b=4
-print(a+b)
+
+c=3
+d=56
+def aux(a, b, c):
+    if(a>b and a>c):
+        return a;
+    
 
 ## Aqui se agrga un comentario
 # deberia ir 8 en vez de 3 en la variable a
